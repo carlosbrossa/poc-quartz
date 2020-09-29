ac4f10d98b9d7ebf4139a7fba711e96899e2c092
carlitos <carlos.brossa@dasa.com.br>
2020-09-28 22:46:17 -0300

    Initial commit

---
+++ src/main/java/com/carlitos/pocQuartz/PocQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.carlitos.pocQuartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocQuartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlitos/pocQuartz/PocQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlitos.pocQuartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
